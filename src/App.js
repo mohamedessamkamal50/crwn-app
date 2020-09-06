9846b9af6670325dfc3807e3d680bede6b4c4514
mohamedessamkamal50 <mohamedessamkamal50@gmail.com>
2020-09-06 19:49:23 +0200

    updating our app with a shop page, collection previwe component & collection item component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
  
 import HomePage from './pages/homepages/homepage.component';
 
-const HatePage = (props) => {
-  console.log(props);
-  return(<div>
-    <h1>Hate Page</h1>
-  </div>
-  )
-};
+
 
 function App() {
   return (
     <div >
       <Switch>
       <Route exact path='/' component ={HomePage} />
-      <Route  path='/hats' component ={HatePage} />
+      <Route  path='/shop' component ={ShopPage} />
       </Switch>
     </div>
   );
